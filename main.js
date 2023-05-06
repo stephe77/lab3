5da9e6697cdd8d10064d86ea2604c5d1f82e5d87
stephe77 <stefnlv@mail.ru>
2023-05-06 10:48:39 +0300

    3

--- main.js
+++ main.js
@@ -7,13 +7,7 @@ const passwordInput = document.getElementById('passwordInput');
 const errorText = document.getElementById('errorText');
 const errorText_email = document.getElementById('errorText_email');
 
-//вывод в консоль
-function printConsole(){
-    let formData = new FormData(registrationForm); 
-    for(let [name, value] of formData) {
-        console.log(`${name} = ${value}`); 
-    }
-}
+
 
 registrationButton.addEventListener('click', () => {
     registrationForm.toggleAttribute('hidden');
@@ -29,23 +23,20 @@ registrationForm.addEventListener('submit', (e) => {
     
 });
 
-//Показать пароль и скрыть 
+
 showPasswordButton.addEventListener('pointerdown', ()=>{
     passwordInput.setAttribute('type', 'text')
-    bShowPass.innerText = 'Скрыть пароль'
 });
 showPasswordButton.addEventListener('pointerup', ()=>{
     passwordInput.setAttribute('type', 'password')
-    bShowPass.innerText = 'Показать пароль'
 });
 
-//Функция проверки полей формы
+
 function validInputEM() {
     errorText.hidden =true;
     emailInput.setCustomValidity('');
     errorText_email.innerText = '';
 
-    //check email validity
     if (!emailInput.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {  
         emailInput.setCustomValidity('Неверный email');
         errorText_email.innerText = 'Неверный email';
@@ -54,12 +45,12 @@ function validInputEM() {
     }  
     return true;
 }
+
+
 function validInputPAS() {
     errorText.hidden =true;
     passwordInput.setCustomValidity('');
     errorText.innerText = '';
-
-    //check passwors length < 6
     if (passwordInput.value.length < 6) {
         passwordInput.setCustomValidity('Пароль должен содержать не менее 6 символов');
         errorText.innerText = 'Пароль должен содержать не менее 6 символов';
@@ -69,10 +60,9 @@ function validInputPAS() {
     return true;
 }
 
-//проверка при потери фокуса
+
 emailInput.addEventListener('blur', validInputEM);
 passwordInput.addEventListener('blur', validInputPAS);
-
 
 document.body.addEventListener('click', (e)=>{
     if (!registrationForm.contains(e.target) && ! registrationButton.contains(e.target)) {
